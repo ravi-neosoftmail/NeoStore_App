45e0d4d74d05f8a644cb60506b0d8081c5a7daa0
ravi-neosoftmail <ravi.ranjan@neosoftmail.com>
2021-08-02 21:35:49 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () =>  {
+
+
+  return (
+    <SafeAreaView>
+      <Text>App.js</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
